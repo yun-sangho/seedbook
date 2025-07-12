e32fb82102e59a2f186d9121b7b6eaa78a5ee6bf
yun-sangho <contact@yunsangho.com>
2025-07-12 21:45:25 +0900

    delete remove usused packages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,4 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Gradient } from "@repo/ui/gradient";
-import { TurborepoLogo } from "@repo/ui/turborepo-logo";
 
 const LINKS = [
   {
@@ -66,22 +63,8 @@ export default function Page() {
                 width={614}
               />
             </div>
-            <div className="absolute z-50 flex items-center justify-center w-64 h-64">
-              <Gradient
-                className="opacity-90 w-[120px] h-[120px]"
-                conic
-                small
-              />
-            </div>
-
-            <div className="flex justify-center items-center z-50">
-              <TurborepoLogo />
-            </div>
           </div>
-          <Gradient
-            className="top-[-500px] opacity-[0.15] w-[1000px] h-[1000px]"
-            conic
-          />
+   
           <div className="z-50 flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6">
             <svg
               className="w-[160px] md:w-[200px] fill-black dark:fill-white"
@@ -102,14 +85,6 @@ export default function Page() {
             </svg>
           </div>
         </div>
-      </div>
-
-      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {LINKS.map(({ title, href, description }) => (
-          <Card href={href} key={title} title={title}>
-            {description}
-          </Card>
-        ))}
       </div>
     </main>
   );
--- packages/ui/src/index.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-
-export * from "./components/ui/select";
-export { cn } from "./utils";
